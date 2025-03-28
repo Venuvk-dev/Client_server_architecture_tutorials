653dc54752d7e9ed4798acfd7cb6752d250cf476
Venuvk0805 <rvenukanth5@gmail.com>
2025-03-28 14:48:26 +0530

    Updated Receiver and sender class

--- src/Tutorial1/Receiver.java
+++ src/Tutorial1/Receiver.java
@@ -1,4 +1,8 @@
 package Tutorial1;
 
 public class Receiver {
+        public void receiveMessage(Message message){
+            System.out.println("Receiver:The message has been received");
+            System.out.println("The message is :"+message.getContent());
+        }
 }
--- src/Tutorial1/Sender.java
+++ src/Tutorial1/Sender.java
@@ -1,4 +1,17 @@
 package Tutorial1;
 
+
 public class Sender {
+    public Message createMessage(String content){
+        Message message=new Message(content);
+        System.out.println("The Message has been created :"+content);
+        return message;
+    }
+
+    public void sendMessage(Message message, Receiver receiver){
+        System.out.println("The message is being sent to the receiver");
+        receiver.receiveMessage(message);
+    }
+
+
 }
