a8bb3176b1e894e583c1e71db7fab2644fa49cb5
Venuvk0805 <rvenukanth5@gmail.com>
2025-03-28 15:02:08 +0530

    Updated the main class as well

--- src/Tutorial1/MessagePassingExample.java
+++ src/Tutorial1/MessagePassingExample.java
@@ -1,4 +1,11 @@
 package Tutorial1;
 
 public class MessagePassingExample {
+    public static void main(String[] args) {
+        Sender sender=new Sender();
+        Receiver receiver=new Receiver();
+
+//        Message message=new Message("Hello This is Venukanth");
+        sender.sendMessage(new Message("Hello This is from Venukanth"),receiver);
+    }
 }
--- src/Tutorial1/Sender.java
+++ src/Tutorial1/Sender.java
@@ -4,12 +4,12 @@ package Tutorial1;
 public class Sender {
     public Message createMessage(String content){
         Message message=new Message(content);
-        System.out.println("The Message has been created :"+content);
+        System.out.println("Sender:The Message has been created :"+content);
         return message;
     }
 
     public void sendMessage(Message message, Receiver receiver){
-        System.out.println("The message is being sent to the receiver");
+        System.out.println("Sender:The message is being sent to the receiver");
         receiver.receiveMessage(message);
     }
 
