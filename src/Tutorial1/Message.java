55af5a3c80853e9e0293b54efb38dbf79ce07e4e
Venuvk0805 <rvenukanth5@gmail.com>
2025-03-28 14:08:05 +0530

    Message  class

---
+++ src/Tutorial1/Message.java
@@ -0,0 +1,13 @@
+package Tutorial1;
+
+public class Message {
+    private String content;
+
+    public Message(String content) {
+        this.content = content;
+    }
+
+    public String getContent(){
+        return content;
+    }
+}
