c79696d2ae4c99de68be83c5f69842d75e4fce5d
Venuvk0805 <rvenukanth5@gmail.com>
2025-03-29 13:56:27 +0530

    Added config logger in the MessagePassingExample.java

--- src/Tutorial1/MessagePassingExample.java
+++ src/Tutorial1/MessagePassingExample.java
@@ -1,11 +1,34 @@
 package Tutorial1;
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class MessagePassingExample {
     public static void main(String[] args) {
-        Sender sender=new Sender();
-        Receiver receiver=new Receiver();
+        configureLogger();  // Call the logger configuration
 
-//        Message message=new Message("Hello This is Venukanth");
-        sender.sendMessage(new Message("Hello This is from Venukanth"),receiver);
+        Sender sender = new Sender();
+        Receiver receiver = new Receiver();
+
+        sender.sendMessage(new Message("Hello This is from Venukanth"), receiver);
+    }
+
+    private static void configureLogger() {
+        try {
+            // Configure global logging level
+            Logger globalLogger = Logger.getLogger("");
+            Handler[] handlers = globalLogger.getHandlers();
+            for (Handler handler : handlers) {
+                globalLogger.removeHandler(handler);
+            }
+            ConsoleHandler consoleHandler = new ConsoleHandler();
+            consoleHandler.setLevel(Level.ALL);
+            globalLogger.addHandler(consoleHandler);
+            globalLogger.setLevel(Level.ALL);
+        } catch (Exception e) {
+            Logger.getGlobal().log(Level.SEVERE, "Error configuring logger: " + e.getMessage(), e);
+        }
     }
 }
